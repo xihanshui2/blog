3f64f6bc005c3bb15315a4d832be6f43f273e8ca
summer <chuanqi.fan@insgeek.com>
2018-07-17 17:12:09 +0800

    测试wkhtmltox

---
+++ app/Http/Controllers/PictureController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PictureController extends Controller
+{
+    //
+    public function index()
+    {
+        echo "hello world";
+    }
+}
